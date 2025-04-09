57ad104040cdb49f0ad40675ea7795fb09a14722
Manuvise088 <mvisentin2008@gmail.com>
2025-04-09 13:20:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const footerMenuIcon = document.getElementById('footer-menu-icon');
+    const fullscreenMenu = document.getElementById('fullscreen-menu');
+    const closeMenuButton = document.getElementById('close-menu');
+
+    // Open menu
+    footerMenuIcon.addEventListener('click', (e) => {
+        e.preventDefault();
+        fullscreenMenu.classList.remove('hide'); // Remove 'hide' class if present
+        fullscreenMenu.classList.add('show'); // Add 'show' class
+    });
+
+    // Close menu
+    closeMenuButton.addEventListener('click', () => {
+        fullscreenMenu.classList.remove('show'); // Remove 'show' class
+        fullscreenMenu.classList.add('hide'); // Add 'hide' class
+    });
+});
