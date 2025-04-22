f86d9fcdf095f4a74c9c8e9e90c03832cf104ee6
Madaan Pratham <pratham.madaan@itc_ceccato.loc>
2025-04-22 12:31:51 +0200

    started page transitions

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const footerMenuIcon = document.getElementById('footer-menu-icon');
     const fullscreenMenu = document.getElementById('fullscreen-menu');
     const closeMenuButton = document.getElementById('close-menu');
+    const ProjectButton = document.getElementById('project');
 
     // Open menu
     footerMenuIcon.addEventListener('click', (e) => {
         e.preventDefault();
-        fullscreenMenu.classList.remove('hide'); // Remove 'hide' class if present
+        fullscreenMenu.classList.remove('hide'); // Remove 'show' class if present
         fullscreenMenu.classList.add('show'); // Add 'show' class
     });
 
@@ -22,4 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
         document.documentElement.style.setProperty('--blur-var', valore);
     });
+
+    ProjectButton.addEventListener('click'), () => {
+        document.getElementById('what').className = 'slideout';
+        document.getElementById('goals').className = 'slideout';
+    }
 });
