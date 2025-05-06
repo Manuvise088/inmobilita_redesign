eda72c0180a27157ca1321b11df0b3ead5ea69df
Madaan Pratham <pratham.madaan@itc_ceccato.loc>
2025-05-06 08:09:04 +0200

    removed project js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeMenuButton = document.getElementById('close-menu');
     const ActivityButton = document.getElementById('activity');
     const PartnerButton = document.getElementById('partner');
-    const ProjectButton = document.getElementById('project');
     const HomeButton = document.getElementById('home');
 
     // Set Home button as default
@@ -32,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     ActivityButton.addEventListener('click', () => {
         // Set Activity button as new default
-        document.body.style.height = '150vh';
+        document.body.style.maxHeight = '150vh';
         document.getElementById('separatore_1').style.marginTop = "94vh";
         ActivityButton.classList.add('selected'); // Add 'selected' class to indicate it's active
         ActivityButton.classList.add('sezione_selezionata');
@@ -77,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
     HomeButton.addEventListener('click', () => {
         if (!HomeButton.classList.contains('selected')) {
             document.getElementById('separatore_1').style.marginTop = "94vh";
-            document.body.style.height = '230vh';
+            document.body.style.maxHeight = '230vh';
             if(ActivityButton.classList.contains('selected')){
                 document.getElementById('activity_div').style.animation = 'slideout 1s forwards 0s 1 normal';
                 ActivityButton.classList.remove('selected');
@@ -98,16 +97,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     partnerIcon.classList.add('material-icons-outlined');
                 }
             }
-            else if(ProjectButton.classList.contains('selected')){
-                document.getElementById('why').style.animation = 'slideout 1s forwards 0s 1 normal';
-                ProjectButton.classList.remove('selected');
-                ProjectButton.classList.remove('sezione_selezionata');
-                const projectIcon = ProjectButton.querySelector('.material-icons, .material-icons-outlined');
-                if (projectIcon) {
-                    projectIcon.classList.remove('material-icons');
-                    projectIcon.classList.add('material-icons-outlined');
-                }
-            }
 
             document.getElementById('what').style.animation = 'slidein 1s forwards 0s 1 normal';
             document.getElementById('goals').style.animation = 'slidein 1s forwards 0s 1 normal';
@@ -126,7 +115,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Set Activity button as new default
         PartnerButton.classList.add('selected'); // Add 'selected' class to indicate it's active
         PartnerButton.classList.add('sezione_selezionata');
-        document.body.style.height = '140vh';
+        document.body.style.maxHeight    = '140vh';
         document.getElementById('separatore_1').style.marginTop = "128vh";
 
         if(HomeButton.classList.contains('selected')){
@@ -149,66 +138,11 @@ document.addEventListener('DOMContentLoaded', () => {
             if (activityIcon) {
                 activityIcon.classList.remove('material-icons');
                 activityIcon.classList.add('material-icons-outlined');
-            }
-        }
-        else if(ProjectButton.classList.contains('selected')){
-            document.getElementById('why').style.animation = 'slideout 1s forwards 0s 1 normal';
-            ProjectButton.classList.remove('selected');
-            ProjectButton.classList.remove('sezione_selezionata');
-            const projectIcon = ProjectButton.querySelector('.material-icons, .material-icons-outlined');
-            if (projectIcon) {
-                projectIcon.classList.remove('material-icons');
-                projectIcon.classList.add('material-icons-outlined');
             }
         }
 
         document.getElementById('partner_div').style.animation = 'slidein 1s forwards 0s 1 normal';
         document.getElementById('partner_div').style.visibility = 'visible';
-    })
-
-    ProjectButton.addEventListener('click', () =>{
-        ProjectButton.classList.add('selected'); // Add 'selected' class to indicate it's active
-        ProjectButton.classList.remove('sezione_precedente');
-        ProjectButton.classList.add('sezione_selezionata');
-        document.body.style.height = '140vh';
-        document.getElementById('separatore_1').style.marginTop = "128vh";
-
-        if(HomeButton.classList.contains('selected')){
-            document.getElementById('what').style.animation = 'slideout 1s forwards 0s 1 normal';
-            document.getElementById('goals').style.animation = 'slideout 1s forwards 0s 1 normal';
-            HomeButton.classList.remove('selected');
-            HomeButton.classList.remove('sezione_selezionata');
-
-            const homeIcon = HomeButton.querySelector('.material-icons, .material-icons-outlined');
-            if (homeIcon) {
-                homeIcon.classList.remove('material-icons');
-                homeIcon.classList.add('material-icons-outlined');
-            }
-        }
-        else if (ActivityButton.classList.contains('selected')){
-            document.getElementById('activity_div').style.animation = 'slideout 1s forwards 0s 1 normal';
-            ActivityButton.classList.remove('selected');
-            ActivityButton.classList.remove('sezione_selezionata');
-            const activityIcon = ActivityButton.querySelector('.material-icons, .material-icons-outlined');
-            if (activityIcon) {
-                activityIcon.classList.remove('material-icons');
-                activityIcon.classList.add('material-icons-outlined');
-            }
-        }
-        else if(PartnerButton.classList.contains('selected')){
-            document.getElementById('partner_div').style.animation = 'slideout 1s forwards 0s 1 normal';
-            PartnerButton.classList.remove('selected');
-            PartnerButton.classList.remove('sezione_selezionata');
-            const partnerIcon = PartnerButton.querySelector('.material-icons, .material-icons-outlined');
-            if (partnerIcon) {
-                partnerIcon.classList.remove('material-icons');
-                partnerIcon.classList.add('material-icons-outlined');
-            }
-        }
-        
-        document.getElementById('why').style.animation = 'slidein 1s forwards 0s 1 normal';
-        document.getElementById('why').style.visibility = 'visible';
-    })
     })
 
     // Scroll to Top functionality
@@ -252,3 +186,4 @@ document.addEventListener('DOMContentLoaded', () => {
             sezione.classList.add('selected');
         });
     });
+});
