c71603de79f4f35de44408287dc132032e51c0a7
prathix <ritukhanna1717@gmail.com>
2025-05-07 21:39:48 +0200

    fixed mobile navbar and home page

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const PartnerButton = document.getElementById('partner');
     const HomeButton = document.getElementById('home');
     const WebappButton = document.getElementById('webapp');
+    const ActivityMob = document.getElementById('activity_mob');
+    const HomeMob = document.getElementById('home_mob');
+    const WebappMob = document.getElementById('webapp_mob');
 
     // Set Home button as default
     HomeButton.classList.add('selected'); // Add 'selected' class to indicate it's active
@@ -167,6 +170,142 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     WebappButton.addEventListener('click', () => {
+        // Set Webapp button as new default
+        document.body.style.height = 'auto';
+        document.getElementById('separatore_1').style.marginTop = "94vh";
+        WebappButton.classList.add('selected'); // Add 'selected' class to indicate it's active
+        WebappButton.classList.add('sezione_selezionata');
+
+        if(HomeButton.classList.contains('selected')){
+            document.getElementById('what').style.animation = 'slideout 1s forwards 0s 1 normal';
+            document.getElementById('goals').style.animation = 'slideout 1s forwards 0s 1 normal';
+            HomeButton.classList.remove('selected');
+            HomeButton.classList.remove('sezione_selezionata');
+
+            const homeIcon = HomeButton.querySelector('.material-icons, .material-icons-outlined');
+            if (homeIcon) {
+                homeIcon.classList.remove('material-icons');
+                homeIcon.classList.add('material-icons-outlined');
+            }
+        }
+        else if (PartnerButton.classList.contains('selected')){
+            document.getElementById('partner_div').style.animation = 'slideout 1s forwards 0s 1 normal';
+            PartnerButton.classList.remove('selected');
+            PartnerButton.classList.remove('sezione_selezionata');
+            const partnerIcon = PartnerButton.querySelector('.material-icons, .material-icons-outlined');
+            if (partnerIcon) {
+                partnerIcon.classList.remove('material-icons');
+                partnerIcon.classList.add('material-icons-outlined');
+            }
+        }
+        else if (ActivityButton.classList.contains('selected')){
+            document.getElementById('activity_div').style.animation = 'slideout 1s forwards 0s 1 normal';
+            ActivityButton.classList.remove('selected');
+            ActivityButton.classList.remove('sezione_selezionata');
+            const activityIcon = ActivityButton.querySelector('.material-icons, .material-icons-outlined');
+            if (activityIcon) {
+                activityIcon.classList.remove('material-icons');
+                activityIcon.classList.add('material-icons-outlined');
+            }
+        }
+
+        document.getElementById('webapp_div').style.animation = 'slidein 1s forwards 0s 1 normal';
+        document.getElementById('webapp_div').style.visibility = 'visible';
+    });
+
+    ActivityMob.addEventListener('click', () => {
+        // Set Activity button as new default
+        document.body.style.height = 'auto';
+        document.getElementById('separatore_1').style.marginTop = "94vh";
+        ActivityButton.classList.add('selected'); // Add 'selected' class to indicate it's active
+        ActivityButton.classList.add('sezione_selezionata');
+
+        if(HomeButton.classList.contains('selected')){
+            document.getElementById('what').style.animation = 'slideout 1s forwards 0s 1 normal';
+            document.getElementById('goals').style.animation = 'slideout 1s forwards 0s 1 normal';
+            HomeButton.classList.remove('selected');
+            HomeButton.classList.remove('sezione_selezionata');
+
+            const homeIcon = HomeButton.querySelector('.material-icons, .material-icons-outlined');
+            if (homeIcon) {
+                homeIcon.classList.remove('material-icons');
+                homeIcon.classList.add('material-icons-outlined');
+            }
+        }
+        else if (PartnerButton.classList.contains('selected')){
+            document.getElementById('partner_div').style.animation = 'slideout 1s forwards 0s 1 normal';
+            PartnerButton.classList.remove('selected');
+            PartnerButton.classList.remove('sezione_selezionata');
+            const partnerIcon = PartnerButton.querySelector('.material-icons, .material-icons-outlined');
+            if (partnerIcon) {
+                partnerIcon.classList.remove('material-icons');
+                partnerIcon.classList.add('material-icons-outlined');
+            }
+        }
+        else if (WebappButton.classList.contains('selected')){
+            document.getElementById('webapp_div').style.animation = 'slideout 1s forwards 0s 1 normal';
+            WebappButton.classList.remove('selected');
+            WebappButton.classList.remove('sezione_selezionata');
+            const webappIcon = WebappButton.querySelector('.material-icons, .material-icons-outlined');
+            if (webappIcon) {
+                webappIcon.classList.remove('material-icons');
+                webappIcon.classList.add('material-icons-outlined');
+            }
+        }
+
+        document.getElementById('activity_div').style.animation = 'slidein 1s forwards 0s 1 normal';
+        document.getElementById('activity_div').style.visibility = 'visible';
+    });
+
+    HomeMob.addEventListener('click', () => {
+        if (!HomeButton.classList.contains('selected')) {
+            document.getElementById('separatore_1').style.marginTop = "94vh";
+            document.body.style.height = 'auto';
+            if(ActivityButton.classList.contains('selected')){
+                document.getElementById('activity_div').style.animation = 'slideout 1s forwards 0s 1 normal';
+                ActivityButton.classList.remove('selected');
+                ActivityButton.classList.remove('sezione_selezionata');
+                const activityIcon = ActivityButton.querySelector('.material-icons, .material-icons-outlined');
+                if (activityIcon) {
+                    activityIcon.classList.remove('material-icons');
+                    activityIcon.classList.add('material-icons-outlined');
+                }
+            }
+            else if(PartnerButton.classList.contains('selected')){
+                document.getElementById('partner_div').style.animation = 'slideout 1s forwards 0s 1 normal';
+                PartnerButton.classList.remove('selected');
+                PartnerButton.classList.remove('sezione_selezionata');
+                const partnerIcon = PartnerButton.querySelector('.material-icons, .material-icons-outlined');
+                if (partnerIcon) {
+                    partnerIcon.classList.remove('material-icons');
+                    partnerIcon.classList.add('material-icons-outlined');
+                }
+            }
+            else if (WebappButton.classList.contains('selected')){
+                document.getElementById('webapp_div').style.animation = 'slideout 1s forwards 0s 1 normal';
+                WebappButton.classList.remove('selected');
+                WebappButton.classList.remove('sezione_selezionata');
+                const webappIcon = WebappButton.querySelector('.material-icons, .material-icons-outlined');
+                if (webappIcon) {
+                    webappIcon.classList.remove('material-icons');
+                    webappIcon.classList.add('material-icons-outlined');
+                }
+            }
+
+            document.getElementById('what').style.animation = 'slidein 1s forwards 0s 1 normal';
+            document.getElementById('goals').style.animation = 'slidein 1s forwards 0s 1 normal';
+            HomeButton.classList.add('selected');
+            HomeButton.classList.add('sezione_selezionata');
+
+            const homeIcon = HomeButton.querySelector('.material-icons, .material-icons-outlined');
+            if (homeIcon) {
+                homeIcon.classList.remove('material-icons-outlined');
+                homeIcon.classList.add('material-icons');
+            }
+        }
+    });
+
+    WebappMob.addEventListener('click', () => {
         // Set Webapp button as new default
         document.body.style.height = 'auto';
         document.getElementById('separatore_1').style.marginTop = "94vh";
