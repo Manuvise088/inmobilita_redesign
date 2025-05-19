a541a62a6a35a78ee5a83cc0c0499455ec899c0d
prathix <ritukhanna1717@gmail.com>
2025-05-19 15:34:26 +0200

    added schools

--- script.js
+++ script.js
@@ -634,7 +634,7 @@ document.addEventListener('DOMContentLoaded', () => {
         PartnerButton.classList.add('selected'); // Add 'selected' class to indicate it's active
         PartnerButton.classList.add('sezione_selezionata');
         document.body.style.height = 'auto';
-        document.getElementById('separatore_1').style.marginTop = "128vh";
+        document.getElementById('separatore_1').style.marginTop = "184vh";
 
         if(HomeButton.classList.contains('selected')){
             document.getElementById('what').style.animation = 'slideout 1s forwards 0s 1 normal';
